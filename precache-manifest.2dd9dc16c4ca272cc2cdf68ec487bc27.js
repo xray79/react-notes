1036f2dcf81ba88a92738c470290d5c739472ee3
Ibnul Huq <ibnul.huq@gmail.com>
2022-03-23 11:10:26 +0000

    Updates

---
+++ precache-manifest.2dd9dc16c4ca272cc2cdf68ec487bc27.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95041408c8e3b9d95ca1693048284a20",
+    "url": "/react-notes/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/react-notes/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "978dcc6a5d8e7257e1a2",
+    "url": "/react-notes/static/js/main.58763910.chunk.js"
+  },
+  {
+    "revision": "51abfc68b84f07a972b2",
+    "url": "/react-notes/static/js/runtime-main.fe960ac6.js"
+  }
+]);
