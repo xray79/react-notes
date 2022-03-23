aaf499b848f910909a9590e7e49525f8bccef8ee
Ibnul Huq <ibnul.huq@gmail.com>
2022-03-23 10:20:55 +0000

    add deployment code

---
+++ build/precache-manifest.39b2b9411eec9bf1e4db15b210f0dd41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34671ec61f00400ad575c307bef5a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "978dcc6a5d8e7257e1a2",
+    "url": "/static/js/main.58763910.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
